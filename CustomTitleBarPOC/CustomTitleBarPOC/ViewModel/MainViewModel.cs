42647d1b25c306c4558997dd4d0faf8b1ff080a8
David Jaroš - DJ <david.jaros@chyron.com>
2023-11-27 15:25:40 +0100

    basic icons set

--- CustomTitleBarPOC/CustomTitleBarPOC/ViewModel/MainViewModel.cs
+++ CustomTitleBarPOC/CustomTitleBarPOC/ViewModel/MainViewModel.cs
@@ -7,9 +7,23 @@ namespace CustomTitleBarPOC.ViewModel
 	public class MainViewModel
 	{
 		public ICommand DragWindowCommand { get; set; }
+
+		public ICommand MinimizeCommand { get; set; }
+
+		public ICommand MaximizeCommand { get; set; }
+
+		public ICommand CloseCommand { get; set; }
+
+		public ICommand InstallUpdateCommand { get; set;}
+
+
 		public MainViewModel()
 		{
 			DragWindowCommand = new RelayCommand(DragWindow, CanExecute);
+			MinimizeCommand = new RelayCommand(MinimizeWindow, CanExecute);
+			MaximizeCommand = new RelayCommand(MaximizeWindow, CanExecute);
+			CloseCommand = new RelayCommand(CloseWindow, CanExecute);
+			InstallUpdateCommand = new RelayCommand(InstallUpdate, CanExecute);
 		}
 
 		private void DragWindow(object parameter)
@@ -18,6 +32,33 @@ namespace CustomTitleBarPOC.ViewModel
 			{
 				Application.Current.MainWindow.DragMove();
 			}
+		}
+
+		private void MinimizeWindow(object parameter)
+		{
+			Application.Current.MainWindow.WindowState = WindowState.Minimized;
+		}
+
+		private void MaximizeWindow(object parameter)
+		{
+			if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+			{
+				Application.Current.MainWindow.WindowState = WindowState.Normal;
+			}
+			else
+			{
+				Application.Current.MainWindow.WindowState = WindowState.Maximized;
+			}
+		}
+
+		private void CloseWindow(object parameter)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void InstallUpdate(object parameter)
+		{
+			// update Prime app
 		}
 
 		private bool CanExecute()
