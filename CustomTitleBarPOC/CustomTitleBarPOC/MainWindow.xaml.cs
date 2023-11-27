1e45a3cfae3d0a5bc9174af6ff96af4f8c5aa625
David Jaroš - DJ <david.jaros@chyron.com>
2023-11-27 12:47:49 +0100

    setup simple custom window

---
+++ CustomTitleBarPOC/CustomTitleBarPOC/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace CustomTitleBarPOC
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+			{
+				this.DragMove();
+			}
+		}
+	}
+}
