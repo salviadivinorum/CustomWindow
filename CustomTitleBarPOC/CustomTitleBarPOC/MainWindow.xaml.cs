752999fe61a7977166732567fc4c314a88503dea
David Jaroš - DJ <david.jaros@chyron.com>
2023-12-05 12:26:40 +0100

    color tweaks

--- CustomTitleBarPOC/CustomTitleBarPOC/MainWindow.xaml.cs
+++ CustomTitleBarPOC/CustomTitleBarPOC/MainWindow.xaml.cs
@@ -2,41 +2,24 @@
 using CustomTitleBarPOC.ViewModel;
 using System;
 using System.Windows;
-using System.Windows.Input;
 
 namespace CustomTitleBarPOC
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-            //WindowSizing.ExtendFrameIntoClientArea(this);
-            //WindowSizing.HandleLocationChanged(this);
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
             DataContext = new MainViewModel();
-
-			this.SourceInitialized += Window_SourceInitialized;
-		}
+            SourceInitialized += Window_SourceInitialized;
+        }
 
         void Window_SourceInitialized(object sender, EventArgs e)
         {
             WindowSizing.WindowInitialized(this);
         }
-
-        /// <summary>
-        /// Code behind version of Drag window functionality
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-		{
-			//if (e.ChangedButton == MouseButton.Left)
-			//{
-			//	DragMove();
-			//}
-		}
-	}
+    }
 }
