f95330d2a48dfed81a7b0c40558b3ea470516cac
David Jaroš - DJ <david.jaros@chyron.com>
2024-01-30 15:40:10 +0100

    System menu implemented as simple context menu

--- CustomTitleBarPOC/CustomTitleBarPOC/View/ChyronWindowTitleBar.xaml.cs
+++ CustomTitleBarPOC/CustomTitleBarPOC/View/ChyronWindowTitleBar.xaml.cs
@@ -1,81 +1,102 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using CustomTitleBarPOC.Extensions;
 
 namespace CustomTitleBarPOC.View
 {
-    /// <summary>
-    /// Interaction logic for ChyronWindowTitleBar.xaml
-    /// </summary>
-    public partial class ChyronWindowTitleBar : System.Windows.Controls.UserControl
+	/// <summary>
+	/// Interaction logic for ChyronWindowTitleBar.xaml
+	/// </summary>
+	public partial class ChyronWindowTitleBar : System.Windows.Controls.UserControl
 	{
-        private Window window;
+		private Window window;
 
-        public ChyronWindowTitleBar()
-        {
-            InitializeComponent();
+		public ChyronWindowTitleBar()
+		{
+			InitializeComponent();
 
-            DataContext = this;
-
-            MouseLeftButtonDown += OnTitleBarLeftButtonDown;
-            MouseDoubleClick += TitleBar_MouseDoubleClick;
-            Loaded += ChyronWindowTitleBar_Loaded;
+			DataContext = this;
+			windowIcon.MouseDown += WindowIcon_MouseDown;
+			MouseLeftButtonDown += OnTitleBarLeftButtonDown;
+			MouseDoubleClick += TitleBar_MouseDoubleClick;
+			Loaded += ChyronWindowTitleBar_Loaded;
 			MouseMove += ChyronWindowTitleBar_MouseMove;
-        }
+		}
 
-		
+
+		private void WindowIcon_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.LeftButton == MouseButtonState.Pressed)
+			{
+				// standard invocation of window system menu
+				// do not use it
+				//var actualTop = WindowHelper.GetWindowTop(window);
+				//var actualLeft = WindowHelper.GetWindowLeft(window);
+
+				//var point = new Point(actualLeft + windowIcon.Width, actualTop + windowIcon.Height);
+				//SystemCommands.ShowSystemMenu(window, point);
+
+				ShowContextMenu();
+			}
+		}
+
+		private ContextMenu? SystemMenu
+		{
+			get
+			{
+				return Resources["systemMenu"] as ContextMenu;
+			}
+		}
+
+		private void ShowContextMenu()
+		{
+			if (SystemMenu != null)
+			{
+				SystemMenu.IsOpen = true;
+			}
+		}
 
 		private void ChyronWindowTitleBar_Loaded(object sender, RoutedEventArgs e)
-        {
-            window = TemplatedParent as Window;
-            if (window != null)
-            {
-                window.StateChanged += Window_StateChanged;
-                RefreshMaximizeRestoreButton(window.WindowState);
-            }
-        }
-       
+		{
+			window = TemplatedParent as Window;
+			if (window != null)
+			{
+				window.StateChanged += Window_StateChanged;
+				RefreshMaximizeRestoreButton(window.WindowState);
+			}
+		}
 
-        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(ChyronWindowTitleBar));
 
-        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(ImageSource), typeof(ChyronWindowTitleBar));
+		public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(ChyronWindowTitleBar));
 
-        public string Title
-        {
-            get { return (string)GetValue(TitleProperty); }
-            set { SetValue(TitleProperty, value); }
-        }
+		public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(ImageSource), typeof(ChyronWindowTitleBar));
 
-        public ImageSource Icon
-        {
-            get { return (ImageSource)GetValue(IconProperty); }
-            set { SetValue(IconProperty, value); }
-        }
+		public string Title
+		{
+			get { return (string)GetValue(TitleProperty); }
+			set { SetValue(TitleProperty, value); }
+		}
 
-        private void TitleBar_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            MaxButton_Click(sender, e);
-        }
+		public ImageSource Icon
+		{
+			get { return (ImageSource)GetValue(IconProperty); }
+			set { SetValue(IconProperty, value); }
+		}
+
+		private void TitleBar_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			MaxButton_Click(sender, e);
+		}
 
 		private Point mouseDownPoint;
 		bool canMoveMaximizedWindow;
 
 		private void ChyronWindowTitleBar_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
 		{
-			if(window != null && e.LeftButton == MouseButtonState.Pressed)
+			if (window != null && e.LeftButton == MouseButtonState.Pressed)
 			{
 				if (canMoveMaximizedWindow)
 				{
@@ -98,11 +119,11 @@ namespace CustomTitleBarPOC.View
 					double left;
 
 					var half = ActualWidth / 2;
-					if(distL < half)
+					if (distL < half)
 					{
 						left = distL;
 					}
-					else if(distR < half)
+					else if (distR < half)
 					{
 						left = ActualWidth - distR;
 					}
@@ -127,7 +148,7 @@ namespace CustomTitleBarPOC.View
 
 
 		private void OnTitleBarLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
+		{
 			if (window.WindowState == WindowState.Maximized)
 			{
 				canMoveMaximizedWindow = true;
@@ -137,62 +158,62 @@ namespace CustomTitleBarPOC.View
 			{
 				canMoveMaximizedWindow = false;
 			}
-        }
+		}
 
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            window?.Close();
-        }
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			window?.Close();
+		}
 
-        private void MinButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (window != null)
-            {
-                window.WindowState = WindowState.Minimized;
-            }
-        }
+		private void MinButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (window != null)
+			{
+				window.WindowState = WindowState.Minimized;
+			}
+		}
 
-        private void MaxButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (window != null)
-            {
-                if (window.WindowState == WindowState.Maximized)
-                {
-                    window.WindowState = WindowState.Normal;
-                }
-                else
-                {
-                    window.WindowState = WindowState.Maximized;
-                }
+		private void MaxButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (window != null)
+			{
+				if (window.WindowState == WindowState.Maximized)
+				{
+					window.WindowState = WindowState.Normal;
+				}
+				else
+				{
+					window.WindowState = WindowState.Maximized;
+				}
 
 				canMoveMaximizedWindow = false;
-            }
-        }
+			}
+		}
 
-        private void RefreshMaximizeRestoreButton(WindowState windowState)
-        {
-            if (windowState == WindowState.Maximized)
-            {
-                maximizeButton.Visibility = Visibility.Collapsed;
-                restoreButton.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                maximizeButton.Visibility = Visibility.Visible;
-                restoreButton.Visibility = Visibility.Collapsed;
-            }
-        }
+		private void RefreshMaximizeRestoreButton(WindowState windowState)
+		{
+			if (windowState == WindowState.Maximized)
+			{
+				maximizeButton.Visibility = Visibility.Collapsed;
+				restoreButton.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				maximizeButton.Visibility = Visibility.Visible;
+				restoreButton.Visibility = Visibility.Collapsed;
+			}
+		}
 
-        private void Window_StateChanged(object sender, EventArgs e)
-        {
-            var window = (Window)sender;
-            RefreshMaximizeRestoreButton(window.WindowState);
-        }
+		private void Window_StateChanged(object sender, EventArgs e)
+		{
+			var window = (Window)sender;
+			RefreshMaximizeRestoreButton(window.WindowState);
+		}
 
-        private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
-        {
-            window.WindowState = WindowState.Minimized;
-        }
+		private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+		{
+			window.WindowState = WindowState.Minimized;
+		}
 
 		public static readonly DependencyProperty MenuContentProperty = DependencyProperty.Register(
 		"MenuContent", typeof(object), typeof(ChyronWindowTitleBar), new PropertyMetadata(default(object)));
